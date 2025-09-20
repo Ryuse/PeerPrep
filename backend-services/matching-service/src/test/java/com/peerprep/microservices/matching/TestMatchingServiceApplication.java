00c784934aacddb167a2b72fb8e6868e5f156d49
Ryuse <Leemuel01234@gmail.com>
2025-09-20 13:59:53 +0800

    Create base files for matching service

---
+++ backend-services/matching-service/src/test/java/com/peerprep/microservices/matching/TestMatchingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.peerprep.microservices.matching;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestMatchingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(MatchingServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
