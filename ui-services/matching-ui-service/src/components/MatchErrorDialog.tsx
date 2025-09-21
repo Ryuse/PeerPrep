341efed1ab634c775719ee1192e0d0e5a1e504a4
Ryuse <Leemuel01234@gmail.com>
2025-09-21 16:52:34 +0800

    Create Match error dialog

---
+++ ui-services/matching-ui-service/src/components/MatchErrorDialog.tsx
@@ -0,0 +1,40 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+interface MatchErrorDialogProps {
+  onClose: () => void;
+  onRetry?: () => void;
+  autoCloseTime?: number;
+}
+
+const MatchErrorDialog: React.FC<MatchErrorDialogProps> = ({ onClose }) => {
+  return (
+    <Dialog open={true} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-center">
+        <DialogHeader>
+          <DialogTitle className="text-2xl">
+            Oops! Something went wrong.
+          </DialogTitle>
+          <DialogDescription className="mt-2">
+            Please try matching again at a later time.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter className="flex justify-center mt-4">
+          <Button onClick={onClose} className="bg-orange-600 hover:bg-red-700">
+            Okay!
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MatchErrorDialog;
