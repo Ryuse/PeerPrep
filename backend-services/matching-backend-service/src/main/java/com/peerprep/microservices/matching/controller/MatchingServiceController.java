3da7c66461184bf5141e3cdaa217c51d3a5f81a4
Ryuse <Leemuel01234@gmail.com>
2025-10-01 18:54:46 +0800

    Update controller links

--- backend-services/matching-backend-service/src/main/java/com/peerprep/microservices/matching/controller/MatchingServiceController.java
+++ backend-services/matching-backend-service/src/main/java/com/peerprep/microservices/matching/controller/MatchingServiceController.java
@@ -1,6 +1,5 @@
 package com.peerprep.microservices.matching.controller;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.HttpStatus;
@@ -9,11 +8,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,15 +23,15 @@ import com.peerprep.microservices.matching.service.UserPreferenceService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/matching-service")
+@RequestMapping("/api/v1/matching-service")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:5173")
 public class MatchingServiceController {
 
   private final MatchingService matchingService;
   private final UserPreferenceService userPreferenceService;
-  // ---------- [User Preference] ----------
 
+  // ---------- [User Preference] ----------
   /**
    * Updates or creates the user preference for the given user ID.
    *
@@ -44,7 +41,7 @@ public class MatchingServiceController {
    *                              updated preference data
    * @return the updated or newly created {@link UserPreferenceResponse}
    */
-  @PutMapping("/{userId}")
+  @PutMapping("preferences/{userId}")
   @ResponseStatus(HttpStatus.OK)
   public UserPreferenceResponse updateUserPreference(
       @PathVariable String userId,
@@ -57,7 +54,7 @@ public class MatchingServiceController {
    *
    * @param userId the ID of the user whose preference is to be deleted
    */
-  @DeleteMapping("/{userId}")
+  @DeleteMapping("preferences/{userId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteUserPreference(@PathVariable String userId) {
     userPreferenceService.deleteUserPreference(userId);
@@ -70,7 +67,7 @@ public class MatchingServiceController {
    * @return the {@link UserPreferenceResponse} containing the user's preference
    * @throws UserPreferenceNotFoundException if the user preference does not exist
    */
-  @GetMapping("/{userId}")
+  @GetMapping("preferences/{userId}")
   @ResponseStatus(HttpStatus.OK)
   public UserPreferenceResponse getUserPreference(@PathVariable String userId) {
     return userPreferenceService.getUserPreference(userId);
@@ -81,15 +78,13 @@ public class MatchingServiceController {
    * Attempts to find a match for a user asynchronously.
    * 
    * If a compatible match exists in the matching pool, the future completes
-   * immediately
-   * with a {@link ResponseEntity} containing the matched user. Otherwise, the
-   * user is added to the
-   * pool and waits asynchronously until a match is found or the timeout expires.
-   * </p>
+   * immediately with a {@link ResponseEntity} containing the matched user.
+   * Otherwise, the user is added to the pool and waits asynchronously until
+   * a match is found or the timeout expires.
    * 
    * If no match is found within the timeout, the future completes with a
-   * {@link ResponseEntity}
-   * with HTTP status 202 (Accepted) indicating that the match request was valid.
+   * {@link ResponseEntity} with HTTP status 202 (Accepted) indicating that the
+   * match request was valid.
    *
    * @param userPreferenceRequest the {@link UserPreferenceRequest} containing the
    *                              user's matching preferences
@@ -98,7 +93,7 @@ public class MatchingServiceController {
    *         the matched user if found, or a 202 Accepted response if no match is
    *         found within the timeout
    */
-  @PutMapping("/request-match/{userId}")
+  @PutMapping("/matches")
   public CompletableFuture<ResponseEntity<?>> requestMatch(@RequestBody UserPreferenceRequest userPreferenceRequest) {
 
     long timeoutMs = 30_000;
@@ -123,16 +118,13 @@ public class MatchingServiceController {
    * Cancels a pending match request for the specified user.
    * 
    * If the user has a pending match request in the matching pool, it will be
-   * removed and
-   * any associated {@link CompletableFuture} will be completed with {@code null}
-   * to indicate
-   * cancellation.
-   * </p>
+   * removed and any associated {@link CompletableFuture} will be completed
+   * with {@code null} to indicate cancellation.
    *
    * @param userId the ID of the user whose pending match request should be
    *               cancelled
    */
-  @DeleteMapping("/cancel-match/{userId}")
+  @DeleteMapping("/matches/{userId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void cancelMatch(@PathVariable String userId) {
     matchingService.cancelMatchRequest(userId);
