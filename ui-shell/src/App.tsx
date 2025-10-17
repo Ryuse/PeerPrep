7f3d0651917967e9e75312c07ece3b08ea46074d
Ryuse <Leemuel01234@gmail.com>
2025-10-17 22:02:22 +0800

    Fix Formatting

--- ui-shell/src/App.tsx
+++ ui-shell/src/App.tsx
@@ -40,7 +40,7 @@ export default function App() {
       .catch(() => setUser(null))
       .finally(() => setLoading(false));
   }, [setUser, setLoading]);
-  
+
   return (
     <Routes>
       {/* Public access */}
